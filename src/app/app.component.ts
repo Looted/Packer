24590b667a04caab005e4b76bf79b895a754b415
Piotr Franczyk <pfranczyk87@gmail.com>
2021-12-02 00:15:15 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,142 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  Activity,
+  ActivityType,
+  HasName,
+  Item,
+  ItemCategory,
+  Modifier,
+} from './models';
+import * as R from 'ramda';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  selectedActivities: string[] = [];
+  selectedModifiers: Modifier[] = [];
+
+  activities: Activity[] = [
+    {
+      name: 'Podróż zagraniczna',
+      types: [ActivityType.Travel],
+      modifiers: [Modifier.Lato],
+      items: [
+        { name: 'Lokalna waluta / revolut', category: ItemCategory.Inne },
+        { name: 'Paszport / dokumenty', category: ItemCategory.Inne },
+        { name: 'Aparat', category: ItemCategory.Elektronika },
+        { name: 'Ładowarka', category: ItemCategory.Elektronika },
+        { name: 'Bielizna', category: ItemCategory.Ubrania },
+        { name: 'Spodnie długie', category: ItemCategory.Ubrania },
+        { name: 'Spodnie krótkie', category: ItemCategory.Ubrania },
+        { name: 'Koszule', category: ItemCategory.Ubrania },
+        { name: 'T-Shirt', category: ItemCategory.Ubrania },
+        { name: 'Szczoteczka do zębów', category: ItemCategory.Inne },
+      ],
+    },
+    {
+      name: 'Trekking',
+      types: [ActivityType.Sport],
+      modifiers: [Modifier.Lato],
+      items: [
+        { name: 'Kurtka przeciwdeszczowa', category: ItemCategory.Ubrania },
+        { name: 'Buty trekkingowe', category: ItemCategory.Ubrania },
+        { name: 'Spodnie trekkingowe', category: ItemCategory.Ubrania },
+        { name: 'T-Shirt trekkingowy', category: ItemCategory.Ubrania },
+        { name: 'Bielizna trekkingowa', category: ItemCategory.Ubrania },
+        { name: 'Czapka', category: ItemCategory.Ubrania },
+        { name: 'Plecak', category: ItemCategory.Inne },
+        { name: 'Czołówka', category: ItemCategory.Elektronika },
+        { name: 'Powerbank', category: ItemCategory.Elektronika },
+      ],
+    },
+    {
+      name: 'Via Ferrata',
+      types: [ActivityType.Sport],
+      modifiers: [Modifier.Lato],
+      items: [
+        {
+          name: 'Rękawiczki skórzane bez palców',
+          category: ItemCategory.Ubrania,
+        },
+        { name: 'Plecak', category: ItemCategory.Inne },
+        { name: 'Czołówka', category: ItemCategory.Elektronika },
+      ],
+    },
+  ];
+
+  possibleItems: Item[] = [];
+  itemsGroupedByCategory: Record<ItemCategory, Item[]>;
+  selectedItems: Item[] = [];
+
+  ngOnInit() {
+    this.selectedActivities =
+      JSON.parse(window.localStorage.getItem('selectedActivities')) || [];
+    this.selectedItems =
+      JSON.parse(window.localStorage.getItem('selectedItems')) || [];
+    this.recalculateItems();
+  }
+
+  toggleActivity(activityName: string) {
+    if (this.selectedActivities.includes(activityName)) {
+      this.selectedActivities = this.selectedActivities.filter(
+        (x) => x !== activityName
+      );
+    } else {
+      this.selectedActivities = [...this.selectedActivities, activityName];
+    }
+
+    window.localStorage.setItem(
+      'selectedActivities',
+      JSON.stringify(this.selectedActivities)
+    );
+
+    this.recalculateItems();
+  }
+
+  toggleItem(item: Item) {
+    if (this.selectedItems.includes(item)) {
+      this.selectedItems = this.selectedItems.filter((x) => x !== item);
+    } else {
+      this.selectedItems = [...this.selectedItems, item];
+    }
+
+    window.localStorage.setItem(
+      'selectedItems',
+      JSON.stringify(this.selectedItems)
+    );
+  }
+
+  recalculateItems() {
+    const selectedActivities = this.activities.filter((x) =>
+      this.selectedActivities.includes(x.name)
+    );
+    this.possibleItems = R.uniqBy(
+      (x) => x.name,
+      R.flatten(R.map((x) => x.items, selectedActivities))
+    );
+
+    this.itemsGroupedByCategory = R.groupBy(
+      (x) => x.category,
+      this.possibleItems
+    );
+  }
+
+  isActivitySelected(activityName: string) {
+    return this.selectedActivities.includes(activityName);
+  }
+
+  isItemSelected(item: Item) {
+    return this.selectedItems.includes(item);
+  }
+
+  categoryTrackBy(category: ItemCategory) {}
+
+  itemTrackBy(index, item: HasName) {
+    return item.name;
+  }
+
+  saveSelection() {}
+}
---
+++ src/app/models.ts
@@ -0,0 +1,33 @@
+export interface HasName {
+  name: string;
+}
+
+export enum Modifier {
+  Lato,
+  Zima,
+  Jesień,
+}
+
+export enum ActivityType {
+  Sport,
+  Travel,
+}
+
+export enum ItemCategory {
+  Ubrania = 'Ubrania',
+  Elektronika = 'Elektronika',
+  Inne = 'Inne',
+}
+
+export class Item implements HasName {
+  name: string;
+  modifiers?: Modifier[];
+  category: ItemCategory;
+}
+
+export class Activity implements HasName {
+  name: string;
+  types: ActivityType[];
+  modifiers: Modifier[];
+  items: Item[];
+}
